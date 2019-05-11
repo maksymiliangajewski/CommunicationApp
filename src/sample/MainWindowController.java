25ff3ea22fc7106692a0ddb8e4c066e1bdd9f63e
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2019-05-11 22:22:47 +0200

    Dialog Pane Added

--- src/sample/MainWindowController.java
+++ src/sample/MainWindowController.java
@@ -1,7 +1,10 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+
+import java.util.Optional;
 
 public class MainWindowController {
     @FXML
@@ -10,5 +13,27 @@ public class MainWindowController {
     {
         System.out.println(username);
         UsernameLabel.setText(username.substring(0,1));
+    }
+    public void displayDialogPane()
+    {
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Hello");
+        dialog.setHeaderText("Create New Help Request");
+        dialog.setResizable(true);
+        Label subject = new Label("Subject: ");
+        Label description = new Label("Description: ");
+        TextField subjectTextField = new TextField();
+        TextField descriptionTextBox = new TextField();
+        GridPane gridPane = new GridPane();
+        gridPane.add(subject, 1,1);
+        gridPane.add(description, 1,2);
+        gridPane.add(subjectTextField, 2,1);
+        gridPane.add(descriptionTextBox, 2,2);
+        dialog.getDialogPane().setContent(gridPane);
+        ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().add(buttonTypeOk);
+        Optional result = dialog.showAndWait();
+
+
     }
 }
