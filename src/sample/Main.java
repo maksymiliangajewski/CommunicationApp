77e96177c2b0febbf7482852359dcf28ec91a038
igork <igorkoncki@gmail.com>
2019-05-11 16:58:18 +0200

    Creating MainWindow of the application

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Pane mainWindowPane = new Pane(root);
+        Scene mainWindowScene = new Scene(mainWindowPane);
+        mainWindowPane.prefHeightProperty().bind(mainWindowScene.heightProperty());
+        mainWindowPane.prefWidthProperty().bind(mainWindowScene.widthProperty());
+        primaryStage.setScene(mainWindowScene);
         primaryStage.show();
     }
 
--- src/sample/MainWindow.java
+++ src/sample/MainWindow.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MainWindow {
+
+    DoubleProperty x;
+
+    public double getX()
+    {
+        return x.get();
+    }
+
+    public void setX(double value)
+    {
+        x.set(value);
+    }
+
+    public DoubleProperty xProperty() {
+        return x;
+    }
+
+    public MainWindow(){
+        x = new SimpleDoubleProperty(10.0);
+    }
 }
